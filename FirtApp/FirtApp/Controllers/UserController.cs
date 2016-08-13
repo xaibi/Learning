64f60ca5cae67dafde535d22b3df3b63322ea377
zohaibdev <zohaibdev@gmail.com>
2016-08-13 04:17:12 -0700

    User successfully added

--- FirtApp/FirtApp/Controllers/UserController.cs
+++ FirtApp/FirtApp/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BussinessObject.Model;
+using FirtApp.Models;
 
 namespace FirtApp.Controllers
 {
@@ -11,8 +12,21 @@ namespace FirtApp.Controllers
     {
         //
         // GET: /User/
+
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+          
+            return View();
+        }
+        [HttpPost]
         public ActionResult AddUser(User user)
         {
+            UserVM userVM = new UserVM();
+           String message  =  userVM.Build(user);
+           TempData["message"] = message;
+
             return View();
         }
 	}
