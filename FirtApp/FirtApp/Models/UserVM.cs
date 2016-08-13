2fca19cac9bb11bda6b9e36d9b73eac2d5fc1e54
zohaibdev <zohaibdev@gmail.com>
2016-08-13 04:18:15 -0700

    add UserVM

---
+++ FirtApp/FirtApp/Models/UserVM.cs
@@ -0,0 +1,26 @@
+﻿using BussinessObject.Model;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirtApp.Models
+{
+    public class UserVM
+    {
+        public String Build(User user)
+        {
+            if (user.Name == null && user.Name.Length < 3)
+            {
+                return "User Name is not Valid";
+            }
+            else
+            {
+                UserRepos userRepos = new UserRepos();
+                userRepos.AddUser(user);
+                return "User successfully added";
+            }   
+        }
+    }
+}
