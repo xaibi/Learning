5cf711a3241d7ae241663adbcb68057665b71520
zohaibdev <zohaibdev@gmail.com>
2016-08-13 04:07:35 -0700

    Add a User Controller

---
+++ FirtApp/FirtApp/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BussinessObject.Model;
+
+namespace FirtApp.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult AddUser(User user)
+        {
+            return View();
+        }
+	}
+}
